5701ab2acec9f11ff8c10e7b5a54bb48446863ff
JohnGu9 <31389215+JohnGu9@users.noreply.github.com>
2022-11-09 11:14:01 +0800

    new demo cases

--- src/stories/LegacyContainerTransform.stories.tsx
+++ src/stories/LegacyContainerTransform.stories.tsx
@@ -1,20 +1,13 @@
 import { action } from '@storybook/addon-actions';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { useState } from 'react';
-import { ContainerTransform, ContainerTransformLayout, ContainerFit } from '../lib/container-transform';
+import { ContainerTransform } from '../lib/container-transform';
+import { ContainerTransformLayout } from '../lib/legacy-container-transform';
 import "./ContainerTransform.css";
 
 export default {
   component: ContainerTransform,
 } as ComponentMeta<typeof ContainerTransform>;
-
-const testText = `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`;
-const container =
-  <div style={{ overflow: 'scroll', height: '100%', width: '100%' }}>
-    <div style={{ padding: 16 }}>{testText}</div>
-  </div>;
 
 const Template: ComponentStory<typeof ContainerTransform> = (args) => {
   const [id, setId] = useState(1 as number | undefined);
@@ -81,14 +74,15 @@ const Template0: ComponentStory<typeof ContainerTransform> = (args) => {
         keyId={id}
         className="container-transform-layout"
         onScrimClick={action('onScrimClick')}
-        overlayPosition={{
-          centerX: 0.6,
-          centerY: 0.7,
-          width: 0.8,
-          height: 0.7,
+        overlayStyle={{
+          bottom: 10,
+          right: 10,
+          width: 100,
+          height: 100,
+          borderRadius: 50,
         }}>
         <ContainerTransform
-          container={container}
+          container={<div className='full-size'>Container</div>}
           style={{
             backgroundColor: 'rgba(186, 186, 186, 1)',
             borderRadius: '10px',
@@ -105,7 +99,7 @@ const Template0: ComponentStory<typeof ContainerTransform> = (args) => {
   );
 }
 
-export const OverlayPosition = Template0.bind({});
+export const OverlayStyle = Template0.bind({});
 
 const Template1: ComponentStory<typeof ContainerTransform> = (args) => {
   const [id, setId] = useState(0 as number | undefined);
@@ -120,7 +114,7 @@ const Template1: ComponentStory<typeof ContainerTransform> = (args) => {
         onScrimClick={action('onScrimClick')}>
         <ContainerTransform
           mock="Mock"
-          container={container}
+          container={<div className='full-size'>Container</div>}
           {...args}
           keyId={0}>
           <div className="container">
@@ -133,45 +127,3 @@ const Template1: ComponentStory<typeof ContainerTransform> = (args) => {
 }
 
 export const Mock = Template1.bind({});
-
-const Template2: ComponentStory<typeof ContainerTransform> = (args) => {
-  const [id, setId] = useState(0 as number | undefined);
-  return (
-    <div className="main">
-      <div className="buttons-bar">
-        <button onClick={() => setId(undefined)}>close</button>
-        <button onClick={() => setId(0)}>open-0</button>
-        <button onClick={() => setId(1)}>open-1</button>
-      </div>
-      <ContainerTransformLayout
-        keyId={id}
-        className="container-transform-layout"
-        onScrimClick={action('onScrimClick')}>
-        <ContainerTransform
-          container={container}
-          containerFit={ContainerFit.width}
-          style={{ backgroundColor: 'white', outline: '1px solid black' }}
-          {...args}
-          keyId={0}>
-          <div className="container">
-            ContainerFit.width
-          </div>
-        </ContainerTransform>
-        <div style={{ height: 32 }} />
-        <ContainerTransform
-          container={container}
-          containerFit={ContainerFit.both}
-          style={{ backgroundColor: 'white', outline: 'dotted' }}
-          {...args}
-          keyId={1}>
-          <div className="container">
-            ContainerFit.both
-          </div>
-        </ContainerTransform>
-      </ContainerTransformLayout>
-    </div>
-  );
-}
-
-
-export const containerFit = Template2.bind({});
